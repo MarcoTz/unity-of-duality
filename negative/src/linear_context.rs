b4695967dbcebfee1a141e9831324dfcfd3c2e4e
MarcoTz <marco.tzschentke@gmail.com>
2024-12-17 16:20:16 +0100

    added linear contexts

--- negative/src/linear_context.rs
+++ negative/src/linear_context.rs
@@ -1,4 +1,5 @@
 use super::formula::{Atom, Formula};
+use std::fmt;
 
 pub enum ContextJudgement {
     Absurd(Atom),
@@ -20,5 +21,27 @@ impl From<ContextJudgement> for LinearContext {
 impl From<Vec<ContextJudgement>> for LinearContext {
     fn from(judgements: Vec<ContextJudgement>) -> LinearContext {
         LinearContext { judgements }
+    }
+}
+
+impl fmt::Display for ContextJudgement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ContextJudgement::Absurd(at) => write!(f, "{at} absurd"),
+            ContextJudgement::True(form) => write!(f, "{form} true"),
+        }
+    }
+}
+
+impl fmt::Display for LinearContext {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(
+            &self
+                .judgements
+                .iter()
+                .map(|judg| format!("{}", judg))
+                .collect::<Vec<String>>()
+                .join(","),
+        )
     }
 }
